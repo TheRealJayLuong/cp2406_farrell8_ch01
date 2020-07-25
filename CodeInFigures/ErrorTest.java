9f63d5fd0d885b61b21cb17d6d0b00dcaddfe6c3
Jay <quocphong.luong@my.jcu.edu.au>
2020-07-25 13:40:32 +1000

    Fix errors in debugging exercises

--- CodeInFigures/ErrorTest.java
+++ CodeInFigures/ErrorTest.java
@@ -1,7 +1,6 @@
 public class ErrorTest
-/*  This class displays a test message 
 {
-   public static void main(String[] args)
+       public static void main(String[] args)
    {
       System.out.println("Test");
    }
